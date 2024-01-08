2656b7ab8d0e61db35b84c606b881928eccd52a5
lavishmanghnani2 <139878421+lavishmanghnani2@users.noreply.github.com>
2024-01-08 19:21:20 +0530

    comment screen

---
+++ lib/screens/comment_controller.dart
@@ -0,0 +1,36 @@
+
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:referral_app/models/home_page_model.dart';
+import 'package:referral_app/repositories/home_pafe_repo.dart';
+
+import '../controller/homeController.dart';
+import '../models/get_comment_model.dart';
+import '../repositories/get_comment_repo.dart';
+
+class GetCommenController extends GetxController{
+  Rx<RxStatus> statusOfGetComment = RxStatus.empty().obs;
+  Rx<GetCommentModel> getCommentModel = GetCommentModel().obs;
+  TextEditingController commentController = TextEditingController();
+  final homeController = Get.put(HomeController());
+  String type='';
+  String id = '';
+  getComment({id, type}){
+    statusOfGetComment.value = RxStatus.empty();
+    getCommentRepo(id: id,type: type).then((value) {
+      statusOfGetComment.value = RxStatus.success();
+      getCommentModel.value = value;
+    });
+  }
+
+
+
+  @override
+  void onInit() {
+    super.onInit();
+    getComment();
+    // getData();
+  }
+}
