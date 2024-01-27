a6ddb715eb7480ff700437559598c5d107a5ad38
lavishmanghnani2 <139878421+lavishmanghnani2@users.noreply.github.com>
2024-01-27 15:29:47 +0530

    1/19/24

--- lib/screens/create_account.dart
+++ lib/screens/create_account.dart
@@ -161,66 +161,91 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
                           onTap: () {
                             _showActionSheet(context);
                           },
-                          child: categoryFile.path != ""
-                              ? Container(
-                            padding: EdgeInsets.all(10),
-                            decoration:
-                            BoxDecoration(
-                              borderRadius:
-                              BorderRadius
-                                  .circular(10),
-                              border: Border.all(color: Colors.black12),
-                              color: Colors.white,
+                          child: Stack(
+                            children: [
+                              categoryFile.path != ""
+                                  ? Container(
+                                padding: EdgeInsets.all(10),
+                                decoration:
+                                BoxDecoration(
+                                  borderRadius:
+                                  BorderRadius
+                                      .circular(10),
+                                  border: Border.all(color: Colors.black12),
+                                  color: Colors.white,
 
-                            ),
-                            margin: const EdgeInsets
-                                .symmetric(
-                                vertical: 10,
-                                horizontal: 10),
-                            width: double.maxFinite,
-                            height: 180,
-                            alignment:
-                            Alignment.center,
-                            child: Image.file(
-                                categoryFile,
-                                errorBuilder: (_, __, ___) =>
-                                    Image.network(
-                                        categoryFile
-                                            .path,
-                                        errorBuilder: (_,
-                                            __,
-                                            ___) =>
-                                        const SizedBox())),
-                          )
-                              : Container(
-                            decoration: BoxDecoration(border: Border.all(color: Colors.black12),color: Colors.white),
-                            padding:
-                            const EdgeInsets.only(
-                                top: 8),
-
-
-                            width: double.maxFinite,
-                            height: 130,
-
-                            child: Column(
-                              mainAxisAlignment:
-                              MainAxisAlignment
-                                  .center,
-                              children: [
-                                Image.asset(
-                                  AppAssets.camera,
-                                  height: 60,
-                                  width: 50,
                                 ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
+                                margin: const EdgeInsets
+                                    .symmetric(
+                                    vertical: 10,
+                                    horizontal: 10),
+                                width: double.maxFinite,
+                                height: 180,
+                                alignment:
+                                Alignment.center,
+                                child: Image.file(
+                                    categoryFile,
+                                    errorBuilder: (_, __, ___) =>
+                                        Image.network(
+                                            categoryFile
+                                                .path,
+                                            errorBuilder: (_,
+                                                __,
+                                                ___) =>
+                                            const SizedBox())),
+                              )
+                                  : Container(
+                                decoration: BoxDecoration(border: Border.all(color: Colors.black12),color: Colors.white),
+                                padding:
+                                const EdgeInsets.only(
+                                    top: 8),
 
-                                const SizedBox(
-                                  height: 11,
+
+                                width: double.maxFinite,
+                                height: 130,
+
+                                child: Column(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment
+                                      .center,
+                                  children: [
+                                    Image.asset(
+                                      AppAssets.camera,
+                                      height: 60,
+                                      width: 50,
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+
+                                    const SizedBox(
+                                      height: 11,
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
+                              ),
+                              Positioned(
+                                top: 15,
+                                right: 15,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    _showActionSheet(context);
+                                  },
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                        color: AppTheme.secondaryColor,
+                                        shape: BoxShape.circle
+                                    ),
+                                    padding: EdgeInsets.all(5),
+                                    child: const Icon(
+                                      Icons.edit,
+                                      color: Colors.white, // You can change the color
+                                      size: 18, // You can change the size
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                         /*    Container(
