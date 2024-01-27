566239521d745d2a718b8661b872558def1cefa9
Prateek Bhardwaj <bhardwaj.prateek17@gmail.com>
2024-01-27 13:20:00 +0530

    new branch

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -357,7 +357,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                 },
                 tabs: [
                   Tab(
-                    child: Text("Recco Feed",
+                    child: Text("Recco Feeds",
                       style: GoogleFonts.mulish(
                         fontWeight: FontWeight.w700, fontSize: 15,
                       ),
